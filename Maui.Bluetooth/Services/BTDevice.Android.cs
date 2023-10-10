61fef9c7e7a1a7338217b102170c315c3a93b1b4
francisco <franke1615@gmail.com>
2023-10-09 18:43:41 -0700

    and we're done with a basic iOS/Android BLE .NET MAUI wrapper.

--- Maui.Bluetooth/Services/BTDevice.Android.cs
+++ Maui.Bluetooth/Services/BTDevice.Android.cs
@@ -5,7 +5,11 @@ namespace Maui.Bluetooth;
 
 public partial class BTDevice : BluetoothGattCallback
 {
+    public BluetoothGatt _gatt; 
     public event EventHandler<BluetoothStateEventArgs> StateChanged;
+
+    private Action<byte[]> _readCharecteristicCompletion;
+    private Action<byte[]> _writeCharecteristicCompletion;
 
     public override void OnConnectionStateChange(
         BluetoothGatt gatt, 
@@ -13,6 +17,7 @@ public partial class BTDevice : BluetoothGattCallback
         [GeneratedEnum] ProfileState newState)
     {
         base.OnConnectionStateChange(gatt, status, newState);
+        _gatt = gatt;
         switch (newState)
         {
             case ProfileState.Connected:
@@ -32,28 +37,151 @@ public partial class BTDevice : BluetoothGattCallback
 
     public partial void DiscoverCharacteristics()
     {
-
+        // only need to discover services, they come with the characteristics already.
+        OnDiscoveredCharacteristics?.Invoke(this, new DiscoveredCharacteristicsArgs
+        {
+            BluetoothDeviceObject = this.OSObject
+        });
     }
     
     public partial void DiscoverServices(string[] serviceUUIDs)
     {
-        BluetoothDevice device = (BluetoothDevice)OSObject;
-        
+        _gatt.DiscoverServices();
+    }
+
+    public override void OnServicesDiscovered(BluetoothGatt gatt, [GeneratedEnum] GattStatus status)
+    {
+        base.OnServicesDiscovered(gatt, status);
+        OnDiscoveredDeviceService?.Invoke(this, new DiscoveredServiceArgs
+        {
+            BluetoothDeviceObject = this.OSObject
+        });
     }
 
     public partial bool HasCharacteristicWithUUID(string uuid)
     {
-        return false;
+        return FindCharacteristicWithUUID(uuid) != null;
     }
 
     public partial void ReadDataFromCharacteristicWithUUID(string uuid, Action<byte[]> completion)
     {
+        _readCharecteristicCompletion = completion;
+        
+        var characteristic = FindCharacteristicWithUUID(uuid);
+        if (characteristic != null)
+        {
+            _gatt.ReadCharacteristic(characteristic);
+        }
+    }
+
+    public override void OnCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, byte[] value, [GeneratedEnum] GattStatus status)
+    {
+        if (OperatingSystem.IsAndroidVersionAtLeast(33))
+        {
+            base.OnCharacteristicRead(gatt, characteristic, value, status);
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                _readCharecteristicCompletion?.Invoke(value);
+            });
+        }
+    }
+
+    public override void OnCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, [GeneratedEnum] GattStatus status)
+    {
+        if (!OperatingSystem.IsAndroidVersionAtLeast(33))
+        {
+            base.OnCharacteristicRead(gatt, characteristic, status);
+            var value = characteristic.GetValue();
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                _readCharecteristicCompletion?.Invoke(value);
+            });
+        }
     }
 
     public partial void SendDataToCharacteristicWithUUID(string uuid, byte[] data, Action<byte[]> completion)
     {
-        
+        _writeCharecteristicCompletion = completion;
+        var characteristic = FindCharacteristicWithUUID(uuid);
+        if (characteristic != null)
+        {
+            if (OperatingSystem.IsAndroidVersionAtLeast(33))
+            {
+                _gatt.WriteCharacteristic(characteristic, data, (int)GattWriteType.Default);
+            }
+            else
+            {
+                characteristic.SetValue(data);
+                _gatt.WriteCharacteristic(characteristic);
+            }
+        }
     }
 
-    public partial void SubscribeToCharacteristicWithUUID(string uuid) { }
+    public override void OnCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, [GeneratedEnum] GattStatus status)
+    {
+        base.OnCharacteristicWrite(gatt, characteristic, status);
+        if (status == GattStatus.Success)
+        {
+            ReadDataFromCharacteristicWithUUID(characteristic.Uuid.ToString(), _writeCharecteristicCompletion);
+        }
+    }
+
+    public partial void SubscribeToCharacteristicWithUUID(string uuid) 
+    {
+        var characteristic = FindCharacteristicWithUUID(uuid);
+        if (characteristic != null)
+        {
+            _gatt.SetCharacteristicNotification(characteristic, true);
+        }
+    }
+
+    public override void OnCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, byte[] value)
+    {
+        if (OperatingSystem.IsAndroidVersionAtLeast(33))
+        {
+            base.OnCharacteristicChanged(gatt, characteristic, value);
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                OnCharacteristicPostedNotification?.Invoke(this, new CharacteristicPostedNotificationArgs
+                {
+                    CharacteristicUUID = characteristic.Uuid.ToString(),
+                    Data = value
+                });
+            });
+        }
+    }
+
+    public override void OnCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic)
+    {
+        if (!OperatingSystem.IsAndroidVersionAtLeast(33))
+        {
+            base.OnCharacteristicChanged(gatt, characteristic);
+            var value = characteristic.GetValue();
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                OnCharacteristicPostedNotification?.Invoke(this, new CharacteristicPostedNotificationArgs
+                {
+                    CharacteristicUUID = characteristic.Uuid.ToString(),
+                    Data = value
+                });
+            });
+        }
+    }
+
+    private BluetoothGattCharacteristic FindCharacteristicWithUUID(string uuid)
+    {
+        var services = _gatt.Services;
+        foreach (var service in services)
+        {
+            foreach (var characteristic in service.Characteristics)
+            {
+                if (characteristic.Uuid.ToString().ToLower() == uuid.ToLower())
+                {
+                    return characteristic;
+                }
+            }
+        }
+
+        return null;
+    }
 }
